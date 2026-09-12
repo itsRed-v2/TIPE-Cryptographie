ec0d1af0b26dc1923d500b40f556e267ccf2f7ea
Paul Charignon <paul.charignon.v2@gmail.com>
2026-09-12 15:41:15 +0200

    Ajout de commentaires à p2pClient.py

--- p2pClient.py
+++ p2pClient.py
@@ -29,6 +29,8 @@ class PacketType(Enum):
     def byte(self):
         return self.value.to_bytes(1)
 
+# Détermine l'adresse à laquelle le PC peut être contacté
+# sur le réseau LOCAL où il est connecté.
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # doesn't even have to be reachable
@@ -39,23 +41,31 @@ def get_ip():
 
 def send_message(sock: socket.socket, message: str, remotePublicKey: bytes):
     remoteKeySize = len(remotePublicKey)
-    msgNumber = textToNumber(message, remoteKeySize)
+    msgNumber = textToNumber(message, remoteKeySize) # convertion du message en entier pour cryptage
     c1, c2 = encrypt(msgNumber, int.from_bytes(remotePublicKey), remoteKeySize)
     content = c1.to_bytes(remoteKeySize) + c2.to_bytes(remoteKeySize)
     length = len(content)
+    # Création du paquet par concaténation de bytes
+    # (2 bytes pour la longueur, 1 byte pour le type de packet, puis le contenu)
     packet = length.to_bytes(2) + PacketType.MESSAGE.byte() + content
     sock.send(packet)
 
 def send_key(sock: socket.socket, key: bytes):
     length = len(key)
+    # Création du paquet par concaténation de bytes
+    # (2 bytes pour la longueur, 1 byte pour le type de packet, puis le contenu)
     packet = length.to_bytes(2) + PacketType.PUBKEY_SHARE.byte() + key
     sock.send(packet)
 
+# Cette fonction, executée dans un thread parallèle, envoie les messages écrits
+# par l'utilisateur.
 def read_stdin(connection: socket.socket, remotePublicKey: bytes):
     while True:
         msg = input().strip()
         send_message(connection, msg, remotePublicKey)
 
+# Cette fonction crée un serveur qui va réceptionner une éventuelle connection
+# de la part d'un ordinateur qui veut communiquer.
 def listen_for_peers():
     HOST_IP = get_ip()
 
@@ -64,18 +74,27 @@ def listen_for_peers():
     serverSocket.listen(0) # become a server socket, only one connection allowed (no backlog).
     print(f"Listening for incoming connections on local network at {HOST_IP}:{HOST_PORT}")
 
+    # La fonction accept est bloquante et retourne un socket
+    # dès qu'une connection est reçue par le serveur.
     connection, address = serverSocket.accept()
     print(f"Received connection from {address}.")
     start_communicating(connection)
 
+# Cette fonction sert à se connecter à un ordinateur qui a créé un serveur
+# avec listen_for_peers
 def connect_to_peer(ip: str, port: int):
     connection = socket.create_connection((ip, port))
-    print(f"Engaged connection with peer {ip}:{port}")
+    print(f"Created connection with peer {ip}:{port}")
     start_communicating(connection)
 
+# Une fois la connection établie, les deux paires entrent dans cette fonction,
+# qui gère les packets reçus.
 def start_communicating(connection: socket.socket):
+    # on demande que recv soit bloquant:
+    # l'execution est en pause tant qu'aucun message n'est recu
     connection.setblocking(True)
 
+    # On envoie sa clé publique à la paire pour qu'elle puisse nous envoyer des messages.
     send_key(connection, hostPublicKey.to_bytes(KEY_SIZE))
 
     key_received = False
@@ -83,13 +102,20 @@ def start_communicating(connection: socket.socket):
     while True:
         try:
             header = connection.recv(3)
+
+            # Si recv retourne 0 bytes, cela veut dire que la connection a été fermée.
             if len(header) == 0:
                 print("Connection closed.")
                 os._exit(0)
+
+            # On vérifie que tous les octets du header ont bien été recus.
             assert len(header) == 3
+            # extraction des données du header: deux bytes pour la taille et 1 pour le type.
             content_size = int.from_bytes(header[0:2])
             packet_type = int.from_bytes(header[2:3])
+            # maintenant qu'on sait la longueur exacte du packet, on peut lire le contenu sur le socket.
             content = connection.recv(content_size)
+            # on vérifie que tous les octets du contenu ont bien été recus.
             assert len(content) == content_size
         except AssertionError:
             print("Error: received incomplete or malformed packet. Exiting.")
@@ -99,8 +125,11 @@ def start_communicating(connection: socket.socket):
                 receive_message(content)
             elif packet_type == PacketType.PUBKEY_SHARE.value and not key_received:
                 receive_key(content, connection)
+                # On note que la clé a été reçue pour ne pas appeler deux fois receive_key,
+                # sinon deux treads seraient créés qui écouteraient stdin simultanément
                 key_received = True
 
+# Décrypte et affiche un message reçu.
 def receive_message(packetContent: bytes):
     c1 = int.from_bytes(packetContent[0:KEY_SIZE])
     c2 = int.from_bytes(packetContent[KEY_SIZE:2*KEY_SIZE])
@@ -113,8 +142,11 @@ def receive_key(packetContent: bytes, connection: socket.socket):
     remotePublicKey = packetContent
     print(f"Received public key from peer. Key size: {remoteKeySize}B.")
     print("You can now send messages to the peer.")
+    # Une fois la clé reçue, on invoque read_stdin dans un thread
+    # parallèle pour permettre à l'utilisateur d'écrire des messages.
     threading.Thread(target=read_stdin, args=(connection, remotePublicKey)).start()
 
+# Cette fonction est executée quand l'utilisateur fait CTRL+C
 def sigint_handler(_sig, _frame):
     print("Exiting.")
     os._exit(0)
@@ -124,15 +156,20 @@ def print_help():
     print(sys.argv[0], "listen")
     print(sys.argv[0], "connect ip:port")
 
-# ===================
-# === ACTUAL CODE ===
-# ===================
+# ============
+# === CODE ===
+# ============
 
+# On dit à python d'appeler sigint_handler quand l'utilisateur fait CTRL+C
 signal.signal(signal.SIGINT, sigint_handler)
 
+# On fait l'action demandée par l'utilisateur, ou on affiche le message d'aide
+# si les arguments sont mal formés.
+# sys.argv[1] est le premier argument de ligne de commande
+# sys.argv[2] est le deuxième.
 if len(sys.argv) == 1:
     print_help()
-if sys.argv[1] == "listen":
+elif sys.argv[1] == "listen":
     listen_for_peers()
 elif sys.argv[1] == "connect":
     try:
